e400f8077335c59a76cebb5e0d218879a0af3126
kristina <kristinagrom876@gmail.com>
2026-02-05 19:02:24 +0700

    переделала

--- laba1.cs
+++ laba1.cs
@@ -1,50 +1,54 @@
 ﻿using System;
 
 class Program {
-  static void Main() {
-    const int numberLength = 2;
-	const int PowerInitialValue = 1;
-	const int SecondDigitIndex = 1;
-	const int DigitsToRemoveCount = 1;
+	static void Main() {
+		int numberLength;
+		int powerInitialValue;
+		int secondDigitIndex;
+		int digitsToRemoveCount;
 
-  while (true) {
-      Console.WriteLine("Select Task:");
-      Console.WriteLine("1 - Raise Number To Power");
-      Console.WriteLine("2 - Second Digit Permutation");
-      Console.Write("Your Choice: ");
+		numberLength = 2;
+		powerInitialValue = 1;
+		secondDigitIndex = 1;
+		digitsToRemoveCount = 1;
 
-      string userChoice = Console.ReadLine();
+		while (true) {
+			Console.WriteLine("Select Task:");
+			Console.WriteLine("1 - Exponentiation");
+			Console.WriteLine("2 - Second Digit Permutation");
+			Console.Write("Your Choice: ");
 
-      if (userChoice == "1") {
-	    Console.Write("a: ");
-	    int baseNumber = int.Parse(Console.ReadLine());
+			string userChoice = Console.ReadLine();
 
-		Console.Write("n: ");
-		int exponentValue = int.Parse(Console.ReadLine());
+			if (userChoice == "1") {
+				Console.Write("a: ");
+				int baseNumber = int.Parse(Console.ReadLine());
+        Console.Write("n: ");
+				int exponentValue = int.Parse(Console.ReadLine());
 
-		long calculationResult = PowerInitialValue;
-		for (int loopCounter = 0; loopCounter < exponentValue; ++loopCounter) {
-		  calculationResult *= baseNumber;
-		}
+				long calculationResult = powerInitialValue;
+				for (int loopCounter = 0; loopCounter < exponentValue; ++loopCounter) {
+					calculationResult *= baseNumber;
+				}
 
-		Console.WriteLine($"Calculation Result: {calculationResult}");
-	  }
-	  else if (userChoice == "2") {
-	    Console.Write("x: ");
-		string inputNumber = Console.ReadLine();
+				Console.WriteLine($"Calculation Result: {calculationResult}");
+			}
+			  else if (userChoice == "2") {
+				  Console.Write("x: ");
+				  string inputNumber = Console.ReadLine();
 
-		if (inputNumber.Length <= numberLength) {
-		  Console.WriteLine("Number Must Contain At Least 3 Digits!");
-		  continue;
-		}
+				  if (inputNumber.Length <= numberLength) {
+					  Console.WriteLine("Number Must Contain At Least 3 Digits!");
+					  continue;
+				  }
 
-		char secondDigit = inputNumber[SecondDigitIndex];
-		string newNumber = inputNumber.Remove(SecondDigitIndex, DigitsToRemoveCount) + secondDigit;
+				  char secondDigit = inputNumber[secondDigitIndex];
+				  string newNumber = inputNumber.Remove(secondDigitIndex, digitsToRemoveCount) + secondDigit;
 
-		  Console.WriteLine($"n: {newNumber}");
-	  } else {
-          Console.WriteLine("Invalid Choice!");
+				  Console.WriteLine($"n: {newNumber}");
+			  } else {
+				  Console.WriteLine("Invalid Choice!");
+			    }
 		}
 	}
-  }
 }
